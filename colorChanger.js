26618d5a9313e90518ee28d111a334c25a87328e
cristinaa45 <cristinavalarezo6@gmail.com>
2025-04-29 12:24:49 -0400

    Update colorChanger.js

--- colorChanger.js
+++ colorChanger.js
@@ -1,5 +1,5 @@
 let button = document.querySelector("#changer");
-let box = document.querySelector("");
+let box = document.querySelector("#button");
 
 button.addEventListener("click", function() {
   // code to change the color goes here
